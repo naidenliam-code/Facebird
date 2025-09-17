2586b2f6a11de780ea4ddbf000bc02f7d61d1e90
naidenliam-code <naidenliam@gmail.com>
2025-09-17 17:14:15 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Service Worker minimal : requis pour l’installabilité */
+/* Service Worker minimal pour installabilité (PWA) */
 self.addEventListener('install', (evt) => {
   self.skipWaiting();
   console.log('[SW] install');
@@ -7,5 +7,5 @@ self.addEventListener('activate', (evt) => {
   self.clients.claim();
   console.log('[SW] activate');
 });
-/* On laisse tout passer pour l’instant (pas de cache obligatoire) */
+/* Pas de stratégie de cache ici (tu as déjà une version avancée si tu veux) */
 self.addEventListener('fetch', () => {});
